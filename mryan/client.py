7abbad016c068e383ab34443d4fa60ac99788d92
mryan0109 <125995609+mryan0109@users.noreply.github.com>
2023-02-26 15:56:07 +0800

    feat:add client

---
+++ mryan/client.py
@@ -0,0 +1,14 @@
+import socket
+
+socket_client = socket.socket()
+socket_client.connect(('127.0.0.1', 8008))
+
+while True:
+    msg = input()
+    if msg == 'exit':
+        break
+    else:
+        socket_client.send(msg.encode("UTF-8"))
+        recv_data = socket_client.recv(1024).decode("UTF-8")
+        print(recv_data)
+socket_client.close()
