6f2e5c75cd75858ee5999ac56327eec56b140145
sanshi <1819620351@qq.com>
2023-02-26 20:21:53 +0800

    feat:add a function which can print "GoodBye!"
    the program can print "GoodBye!" now!

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,9 @@
 public class HelloWorld {
+    public static void hello(){
+        System.out.println("Hello World");
+    }
+
+    public static void goodbye(){
+        System.out.println("GoodBye!");
+    }
 }
